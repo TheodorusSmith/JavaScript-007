20c4ec87f3196bc8334ec4a6d153c3e3b1d91c58
Theo Smith <80037733+TheodorusSmith@users.noreply.github.com>
2021-05-23 21:50:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const makingPizza = function(){
+    console.log("Preheat oven at 200 degrees");
+    console.log("Take pizza from freezer");
+    console.log("Put pizza in oven");
+    console.log("Warm the pizza for 13 minutes");
+    console.log("Get pizza out of the oven");
+    console.log("Bon Apetito");
+};
+const makingSushi = function(){
+    console.log("boil rice");
+    console.log("cut vegetables");
+    console.log("prepare the fish");
+    console.log("Put it together");
+    console.log("Call it Chef's Special");
+};
+
+makingSushi();
+makingSushi();
+makingSushi();
+makingPizza();
+makingPizza();
